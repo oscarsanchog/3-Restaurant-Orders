7c81e402f0602b51010f1cccf8f99d158f3c191d
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-11-11 22:30:33 -0300

    proyecto resuelto

--- index.ts
+++ index.ts
@@ -2,11 +2,40 @@ import { restaurants, Restaurant } from "./restaurants";
 import { orders, Order, PriceBracket } from "./orders";
 
 /// Add your getMaxPrice() function below:
-
+function getMaxPrice(price: PriceBracket) {
+  switch(price) {
+    case PriceBracket.Low:
+      return 10.0
+    case PriceBracket.Medium:
+      return 20.0
+    default: 
+        return 30.0
+  }
+}
 /// Add your getOrders() function below:
+function getOrders(price: PriceBracket, orders: Order[][]) {
+  const filteredOrders: Order[][] = []
+  
+  orders.forEach((restaurant: Order[]) => {
+    const result = restaurant.filter((order: Order) => order.price <= getMaxPrice(price))
+    filteredOrders.push(result)
+  })
+  return filteredOrders
+}
 
 /// Add your printOrders() function below:
+function printOrders(restaurants: Restaurant[], orders: Order[][]) {
+  restaurants.forEach((restaurant: Restaurant, index: number) => {
+    if(orders[index].length) {
+      console.log(restaurant.name)
+      orders[index].forEach(order => {
+        console.log(`- ${order.name}: $${order.price}`)
+      })
+    }
+  })
+}
 
 /// Main
-// const elligibleOrders = getOrders(PriceBracket.Low, orders);
-// printOrders(restaurants, elligibleOrders);
+const elligibleOrders = getOrders(PriceBracket.Low, orders);
+//console.log(elligibleOrders)
+printOrders(restaurants, elligibleOrders);
--- orders.ts
+++ orders.ts
@@ -1,5 +1,5 @@
 export enum PriceBracket {
-    low, // $0 - $10
+    Low, // $0 - $10
     Medium, // $10 - $20
     High, // $20 - $30
    }
@@ -78,3 +78,5 @@ export enum PriceBracket {
    ];
    
    export type Order = typeof orders[0][0];
+   
+   
